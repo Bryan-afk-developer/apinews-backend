2576884ad83b5f925ec65e46c8a7cb7d3ccda1e1
Bryan-afk-developer <bastarracheabryan@gmail.com>
2025-10-18 20:51:09 -0600

    fix: disable SSL in local env

--- ApiNews/config.db.js
+++ ApiNews/config.db.js
@@ -1,30 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-// --- ¡ESTA ES LA CORRECCIÓN CLAVE! ---
-// Le decimos a Node.js que solo intente usar 'dotenv'
-// si NO estamos en el entorno de 'production' (como Vercel).
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-// ------------------------------------
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const connection = new Sequelize(
-    process.env.DB_NAME,     // Vercel/Railway los provee
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT, // Vercel/Railway los provee
-        dialect: 'mysql',
-        
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false 
-          }
-        },
-        logging: false 
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: isProduction
+      ? { ssl: { require: true, rejectUnauthorized: false } } // solo usa SSL en producción
+      : {}, // nada de SSL localmente
+    logging: false,
+  }
 );
 
 module.exports = { connection };
